6e435957251479eaaca4d8ef43cc954a71a56651
Romeo <kilazlove@gmail.com>
2021-11-14 22:42:10 +0800

    initial commit

---
+++ index.py
@@ -0,0 +1,6 @@
+# Create a program that will ask for grade percentage. Display the equivalent Grade/Mark and Description
+# Example:
+# Input grade: 87.6
+# Grade/Mark: 1.75
+# Description: Very Good
+
